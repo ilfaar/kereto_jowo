6a264324a35e174bddf456e2281f72e16c42ebc1
ilfaar <ilfaar@yahoo.co.id>
2019-12-15 15:05:06 +0700

    Update kereto_jowo.py

--- kereto_jowo.py
+++ kereto_jowo.py
@@ -192,8 +192,8 @@ def check_first(checkdata, bookingdata, numretry, usingproxy):
                 if(len(resCheck['payload']) > 0):
                     print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' process -> check ' + str(len(resCheck['payload'])) + ' train')
                     for i in resCheck['payload']:
-                        if i['noka'] == reqcheck['train_no']:
-                            if i['subclass'] == reqcheck['subclass']:
+                        if i['noka'] == (reqcheck['train_no'] or reqcheck['train_no1'] or reqcheck['train_no2'] or reqcheck['train_no3']):
+                            #if i['subclass'] == reqcheck['subclass']:
                                 if i['availability'] >= int(reqcheck['adult']):
                                     print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' hooray seat found process -> check kursi : ' + str(i['availability']))
                                     global checkresult
